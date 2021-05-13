ca615200b6dad8bb7d5a5dcdaa86b7d3a8c3c5cd
lukaBokarica <luka.bokarica1999@gmail.com>
2021-05-13 15:52:42 +0200

    mvp arch for login and quiz table

--- QuizApp/QuizSelectionFeature/Views/QuizzesViewController.swift
+++ QuizApp/QuizSelectionFeature/Views/QuizzesViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class QuizzesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class QuizzesViewController: UIViewController, QuizzesViewDelegate, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var funFactLabel: UILabel!
     
@@ -16,29 +16,27 @@ class QuizzesViewController: UIViewController, UITableViewDelegate, UITableViewD
     @IBOutlet weak var lighbulbImage: UIImageView!
     
     @IBOutlet weak var quizTable: UITableView!
-    
-    var quizzesByCategory = Dictionary<QuizCategory,[Quiz]>()
-    
-    var networkService : NetworkServiceProtocol?
-    
-    var data : [Quiz] = []
-    
-    var quizzes : [Quiz] = []
-    
+
+    private let quizzesPresenter = QuizzesPresenter(networkService: NetworkService())
+        
     @IBAction func getQuizClicked(_ sender: UIButton) {
-        fetchQuizzes()
+        quizzesPresenter.fetchQuizzes()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        quizzesPresenter.setViewDelegate(quizzesViewDelegate: self)
+
+        initialSetup()
+    }
+    
+    func initialSetup() {
         quizTable.register(TableViewCell.nib(), forCellReuseIdentifier: TableViewCell.identifier)
         quizTable.delegate = self
         quizTable.dataSource = self
         quizTable.backgroundColor = .clear
         quizTable.isHidden = true
-        
-        networkService = NetworkService()
         
         funFactLabel.isHidden = true
         lighbulbImage.isHidden = true
@@ -56,74 +54,32 @@ class QuizzesViewController: UIViewController, UITableViewDelegate, UITableViewD
         titleItem.textColor = .white
         titleItem.font = titleItem.font.withSize(20)
         navigationItem.titleView = titleItem
-                
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return quizzesByCategory.keys.count
+        return quizzesPresenter.numberOfSections(in: tableView)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let quizCat = Array(quizzesByCategory.keys)[section]
-        return quizzesByCategory[quizCat]!.count
+        return quizzesPresenter.tableView(tableView, numberOfRowsInSection: section)
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: TableViewCell = tableView.dequeueReusableCell(
-            withIdentifier: TableViewCell.identifier,
-            for: indexPath) as! TableViewCell
-        let quizCat = Array(quizzesByCategory.keys)[indexPath.section]
-        cell.quizNameLabel.text = quizzesByCategory[quizCat]![indexPath.row].title
-        cell.quizDescriptionLabel.text = quizzesByCategory[quizCat]![indexPath.row].description
-        cell.quizDifficultyLabel!.text! = "Difficulty: " + String(quizzesByCategory[quizCat]![indexPath.row].level)
-        cell.selectionStyle = .none
-        
-        return cell
+        return quizzesPresenter.tableView(tableView, cellForRowAt: indexPath)
     }
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        
-        let quizCat = Array(quizzesByCategory.keys)[section]
-        let categoryName = quizCat.rawValue
-        
-        let headerView = UILabel()
-        headerView.text = "    " + categoryName
-        headerView.backgroundColor = UIColor.systemIndigo
-        headerView.textColor = .white
-        headerView.font = UIFont.boldSystemFont(ofSize: 20)
-        
-        return headerView
+        return quizzesPresenter.tableView(tableView, viewForHeaderInSection: section)
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
     {
-        let quizCat = Array(quizzesByCategory.keys)[indexPath.section]
-        let quiz = quizzesByCategory[quizCat]![indexPath.row]
-        let vc = StartQuizViewController(quiz: quiz)
-        self.navigationController?.pushViewController(vc, animated: true)
+        return quizzesPresenter.tableView(tableView, didSelectRowAt: indexPath)
     }
     
-    func getCategories(quizzes : [Quiz]) {
-        var categories = Set<QuizCategory>()
-        
-        for quiz in quizzes {
-            categories.insert(quiz.category)
-        }
+    func pushStartQuiz(startQuizController: StartQuizViewController) {
+        self.navigationController?.pushViewController(startQuizController, animated: true)
     }
-    
-    func getFunFact(quizzes : [Quiz]) -> Int {
-        var stringAppearanceCounter = 0
-        for quiz in quizzes {
-            let quizQuestions = quiz.questions
-            for question in quizQuestions {
-                if(question.question.contains("NBA")) {
-                    stringAppearanceCounter += 1
-                }
-            }
-        }
-        return stringAppearanceCounter
-    }
-    
     
     func showErrorLabel() {
         DispatchQueue.main.async { [self] in
@@ -136,37 +92,16 @@ class QuizzesViewController: UIViewController, UITableViewDelegate, UITableViewD
         }
     }
     
-    func fetchQuizzes() {
-        DispatchQueue.global().async {
-            guard let url = URL(string: "https://iosquiz.herokuapp.com/api/quizzes") else { return }
-            var request = URLRequest(url: url)
-            request.httpMethod = "GET"
-            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-                    
-            self.networkService!.executeUrlRequest(request) {(result: Result<QuizzesResponse, RequestError>) in
-                switch result {
-                case .failure(_):
-                    self.showErrorLabel()
-                case .success(let value):
-                    self.completed(quizzes: value.quizzes)
-                }
-            }
-        }
-    }
-    
     func completed(quizzes: [Quiz]) {
-        self.quizzes = quizzes
+        quizzesPresenter.setQuizzes(quizzes: quizzes)
         
-        quizzesByCategory = Dictionary(grouping: quizzes) { (quiz) -> QuizCategory in
-            return quiz.category
-        }
         quizTable.reloadData()
             
         quizTable.isHidden = false
         funFactLabel.isHidden = false
         lighbulbImage.isHidden = false
         funFactTitleLabel.isHidden = false
-        let funFactNum = self.getFunFact(quizzes: quizzes)
+        let funFactNum = quizzesPresenter.getFunFact(quizzes: quizzes)
         funFactLabel.text = "There are " + String(funFactNum) +
         " quizzes that contain the word NBA."
     }
